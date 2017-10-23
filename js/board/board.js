f6500d786d7afcca902a2134e2c253412b192145
Vorh <sorcerer12@list.ru>
2017-10-23 23:56:21 +0300

    [ EDIT ] board not working

--- js/board/board.js
+++ js/board/board.js
@@ -12,6 +12,27 @@ function Board() {
 
         let table = document.createElement("table");
 
+        let week = 0;
+        let year = 0;
+
+
+        let header = document.createElement("tr");
+        header.className = "board-header";
+
+        for (let i=0;i< column; i++){
+            let th= document.createElement("div");
+
+            if (week===0){
+                th.innerHTML = i;
+                week = 5;
+                header.appendChild(th);
+            }
+
+
+            week--;
+        }
+
+
 
         for (let i = 0; i<row; i++){
 
@@ -28,6 +49,7 @@ function Board() {
 
         }
 
+        parent.appendChild(header);
         parent.appendChild(table);
 
     };
