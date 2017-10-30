b96826a47a2a69ea5e8b06aa3646f8a639b8c8ff
Vorh <sorcerer12@list.ru>
2017-10-31 02:23:03 +0300

    [ ADD ] function for create vertical and horizontal header

--- js/board/board.js
+++ js/board/board.js
@@ -7,6 +7,7 @@
 
 function Board() {
 
+
     const column = 51;
     const row = 90;
 
@@ -15,38 +16,25 @@ function Board() {
         let currentDate = new Date();
 
 
-
         let table = document.createElement("table");
         let header = createHeaderTableHorizontal();
         table.appendChild(header);
 
-        let count = 0;
-        let value = 0;
 
-        for (let i = 0; i < row; i++) {
+        for (let i = 1; i < row; i++) {
 
             let tr = document.createElement("tr");
 
-            let th = document.createElement("th");
-            if(count === 0){
-                th.innerHTML = value;
-                th.className = "board-header vertical";
-                count=4;
-                value +=5;
-            }else {
-                th.innerHTML = "";
-                count--;
-            }
 
-            tr.appendChild(th);
+            tr.appendChild(createHeaderTableVertical(i));
 
 
             for (let q = 1; q < column; q++) {
 
                 let td = document.createElement("td");
-                if (user.dateOfBirth <= currentDate){
+                if (user.dateOfBirth <= currentDate) {
                     td.className = "emptiness";
-                    currentDate.setDate(currentDate.getDate()-7);
+                    currentDate.setDate(currentDate.getDate() - 7);
                 }
                 tr.appendChild(td);
             }
@@ -60,33 +48,55 @@ function Board() {
     };
 }
 
+function createHeaderTableVertical(count) {
+
+    let th = document.createElement("th");
+    let value;
+
+    if (count === 1) {
+        value = 1;
+    }else if (count === 89){
+        value = 90;
+    }else if (count % 5 === 0) {
+        value = count;
+    } else {
+        value = "";
+    }
+
+    th.className = "board-header vertical";
+    th.innerHTML = value;
+
+    return th;
+}
 
 function createHeaderTableHorizontal() {
     let header = document.createElement("tr");
+
+    createHeader("", 1);
+    createHeader(1);
+    createHeader(5, 5);
+    createHeader(10, 5);
+    createHeader(15, 5);
+    createHeader(20, 5);
+    createHeader(25, 5);
+    createHeader(30, 5);
+    createHeader(35, 5);
+    createHeader(40, 5);
+    createHeader(45, 5);
+    createHeader(50, 1);
+
 
     function createHeader(value) {
         let th = document.createElement("th");
         th.className = "board-header horizontal";
         th.innerHTML = value;
-        if (arguments.length === 2){
+        if (arguments.length === 2) {
             th.setAttribute("colspan", arguments[1]);
-        }else {
-            th.setAttribute("colspan", 5);
+        } else {
+            th.setAttribute("colspan", 4);
         }
         header.appendChild(th);
     }
-
-    createHeader("",1);
-    createHeader(0);
-    createHeader(5);
-    createHeader(10);
-    createHeader(15);
-    createHeader(20);
-    createHeader(25);
-    createHeader(30);
-    createHeader(35);
-    createHeader(40);
-    createHeader(45);
 
     return header;
 }
